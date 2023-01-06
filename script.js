8c3bad9af072f41548292c697c983aeacc6e1e72
Mohammad Hussain <95843547+Hussain-ksmd@users.noreply.github.com>
2023-01-06 11:00:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your code here
+const textbox = document.getElementById('evaluatedText');
+const letterCount = document.getElementById('letterCount');
+
+textbox.addEventListener('input', function() {
+  letterCount.textContent = this.value.length;
+});
